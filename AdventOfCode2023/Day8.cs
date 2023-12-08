31df7a1e9c00d4f073dee49a909e8db810ce13d9
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2023-12-08 21:27:03 +0100

    Day 8

--- AdventOfCode2023/Day8.cs
+++ AdventOfCode2023/Day8.cs
@@ -1,12 +1,13 @@
-﻿namespace AdventOfCode2023
+﻿using System.IO;
+
+namespace AdventOfCode2023
 {
     class Day8
     {
-       
-        public static void Run(string[] lines)
+        private static void Part1(string[] lines)
         {
             var steps = lines[0].Trim().Select(c => c == 'L' ? 0 : 1).ToArray();
-            
+
             var nodes = lines.TakeLast(lines.Length - 2)
                 .Select(l => l.Replace("(", "").Replace(")", "").Trim())
                 .Select(l => new
@@ -18,7 +19,6 @@
 
             var nodeidmap = nodes.Select((n, i) => (n, i)).ToDictionary(i => i.n.id, i => i.i);
             var nodeArray = new int[nodeidmap.Count, 2];
-            var index = 0;
             foreach (var node in nodes)
             {
                 nodeArray[nodeidmap[node.id], 0] = nodeidmap[node.lid];
@@ -30,74 +30,16 @@
             {
                 nodeNames[nim.Value] = nim.Key;
             }
-
             long stepsCount = 0;
             int stepIndex = 0;
-            //var startNodeIndex = nodeidmap["AAA"];
-            //var targetNodeIndex = nodeidmap["ZZZ"];
-            //var currNodeIndex = startNodeIndex;
-            //Console.WriteLine($"Target node index: {targetNodeIndex}");
-            //while (true)
-            //{
-            //    if (currNodeIndex == targetNodeIndex)
-            //        break;
-
-            //    var step = steps[stepIndex];
-            //    stepIndex++;
-            //    if (stepIndex >= steps.Length)
-            //    {
-            //        stepIndex = 0;
-            //    }
-            //    stepsCount++;
-            //    currNodeIndex = nodeArray[currNodeIndex, step];
-            //}
-
-            //Console.WriteLine($"Part 1: {stepsCount}");
-
-
-            // look for all nodes that end with 'A'
-            List<int> searchersList = new List<int>();
-            List<int> ends = new List<int>();
-            for (int i = 0; i < nodeNames.Length; i++)
-            {
-                var name = nodeNames[i];
-                if (name[2] == 'A') searchersList.Add(i);
-                else if (name[2] == 'Z') ends.Add(i);
-            }
-
-            Console.WriteLine($"Starting from {searchersList.Count} start points, will look for {ends.Count} end points.");
-
-            stepsCount = 0;
-            stepIndex = 0;
-            var visitFlags = new bool[nodeArray.Length];
-            var searches = searchersList.ToArray();
-
+            var startNodeIndex = nodeidmap["AAA"];
+            var targetNodeIndex = nodeidmap["ZZZ"];
+            var currNodeIndex = startNodeIndex;
+            Console.WriteLine($"Target node index: {targetNodeIndex}");
             while (true)
             {
-                // check all end points vs start points
-                var hasAll = true;
-                var found = ends.Count;
-                for (int i = 0; i < ends.Count; i++)
-                {
-                    if (visitFlags[ends[i]] == false)
-                    {
-                        hasAll = false;
-                        found--;
-                    }
-                }
-                if (found > 2)
-                {
-                    Console.WriteLine($"Found {found} ends out of {ends.Count} - steps count: {stepsCount} - need {string.Join(", ", ends)}, got: {string.Join(", ", searches)}");
-                }
-
-                if (hasAll)
+                if (currNodeIndex == targetNodeIndex)
                     break;
-
-                for (int i = 0; i < searches.Length; i++)
-                {
-                    visitFlags[searches[i]] = false;
-                }
-
 
                 var step = steps[stepIndex];
                 stepIndex++;
@@ -106,20 +48,78 @@
                     stepIndex = 0;
                 }
                 stepsCount++;
-
-                for (int i = 0; i < searches.Length; i++)
-                {
-                    searches[i] = nodeArray[searches[i], step];
-                    visitFlags[searches[i]] = true;
-                }
+                currNodeIndex = nodeArray[currNodeIndex, step];
             }
 
-
-            Console.WriteLine($"Part 2: {stepsCount}");
-
-
+            Console.WriteLine($"Part 1: {stepsCount}");
+        }
 
 
+        private static void Part2(string[] lines)
+        {
+            var steps = lines[0].Trim().Select(c => c == 'L' ? 0 : 1).ToArray();
+
+            var nodes = lines.TakeLast(lines.Length - 2)
+                .Select(l => l.Replace("(", "").Replace(")", "").Trim())
+                .Select(l => new
+                {
+                    id = l.SplitAtAsString('=', 0).Trim(),
+                    lid = l.SplitAtAsString('=', 1).Trim().SplitAtAsString(',', 0),
+                    rid = l.SplitAtAsString('=', 1).Trim().SplitAtAsString(',', 1)
+                });
+
+            var nodeidmap = nodes.Select((n, i) => (n, i)).ToDictionary(i => i.n.id, i => i.i);
+            var nodeArray = new int[nodeidmap.Count, 2];
+            foreach (var node in nodes)
+            {
+                nodeArray[nodeidmap[node.id], 0] = nodeidmap[node.lid];
+                nodeArray[nodeidmap[node.id], 1] = nodeidmap[node.rid];
+            }
+
+            var nodeNames = new string[nodeidmap.Count];
+            foreach (var nim in nodeidmap)
+                nodeNames[nim.Value] = nim.Key;
+
+            // look for all nodes that end with 'A'
+            List<int> searchersList = new();
+            List<int> ends = new();
+            for (int i = 0; i < nodeNames.Length; i++)
+            {
+                var name = nodeNames[i];
+                if (name[2] == 'A') searchersList.Add(i);
+                else if (name[2] == 'Z') ends.Add(i);
+            }
+
+            var visitFlags = new bool[nodeArray.Length];
+            var stepCount = steps.Length;
+            long fctr = 1L;
+            searchersList.Select(l => GetPath(l, 0)).Select(l => l / stepCount)
+                .ForEach(l => fctr *= l);
+
+            Console.WriteLine($"Part 2: {fctr * stepCount}");
+
+            int GetPath(int node, int stepIndex)
+            {
+                int stepsCount = 0;
+                while (true)
+                {
+                    if (ends.Contains(node))
+                        break;
+                    var step = steps[stepIndex];
+                    stepIndex++;
+                    if (stepIndex >= steps.Length)
+                        stepIndex = 0;
+                    stepsCount++;
+                    node = nodeArray[node, step];
+                }
+                return stepsCount;
+            }
+        }
+
+        public static void Run(string[] lines)
+        {
+            Part1(lines);
+            Part2(lines);
         }
     }
 }
