9fc72d8b3d3e904c7bcb3926ee57d69eaed79958
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2023-12-12 19:30:32 +0100

    code rewritten, but still using cache, stack and rec

--- AdventOfCode2023/2023/12/Day12.cs
+++ AdventOfCode2023/2023/12/Day12.cs
@@ -6,8 +6,8 @@
 // https://github.com/encse/adventofcode/blob/master/2023/Day12/Solution.cs
 // I've got first part via brute force permutation generator.
 // but it failed miserably at second part.
-// I still don't understand this code (mostly because it's db linq), but i've got it here to debug and get a grasp of it.
-// it does, however, work
+// I still don't understand this code (mostly because it's db linq, and I am too old for functional programming :),
+// but i've got it here to debug and get a grasp of it.
 
 
 
@@ -19,7 +19,7 @@ namespace AdventOfCode2023
     //[Force] // uncomment to force processing this type
     //[AlwaysEnableLog]
     //[DisableLogInDebug]
-    [UseLiveDataInDeug]
+    //[UseLiveDataInDeug]
     //[AlwaysUseTestData]
     class Day12
     {
@@ -27,98 +27,122 @@ namespace AdventOfCode2023
         public static string LiveFile => "2023\\12\\live.txt";
 
 
-        private static long Solve(string[] input, int repeat) => (
-       from line in input
-       let parts = line.Split(" ")
-       let pattern = Unfold(parts[0], '?', repeat)
-       let numString = Unfold(parts[1], ',', repeat)
-       let nums = numString.Split(',').Select(int.Parse)
-       select
-           Compute(pattern, ImmutableStack.CreateRange(nums.Reverse()), new Cache())
-   ).Sum();
 
-        private static string Unfold(string st, char join, int unfold) =>
-            string.Join(join, Enumerable.Repeat(st, unfold));
 
-        private static long Compute(string pattern, ImmutableStack<int> nums, Cache cache)
+
+        private static string In(int indent) => "".PadLeft(indent);
+        private static int lineSize = 0;
+        private static long Compute(string pattern, ImmutableStack<int> nums, Cache cache, int indent = 0)
         {
-            if (!cache.ContainsKey((pattern, nums)))
+            var key = (pattern, nums);
+            if (cache.TryGetValue(key, out var value))
             {
-                cache[(pattern, nums)] = Dispatch(pattern, nums, cache);
-            }
-            return cache[(pattern, nums)];
-        }
-
-        private static long Dispatch(string pattern, ImmutableStack<int> nums, Cache cache)
-        {
-            return pattern.FirstOrDefault() switch
-            {
-                '.' => ProcessDot(pattern, nums, cache),
-                '?' => ProcessQuestion(pattern, nums, cache),
-                '#' => ProcessHash(pattern, nums, cache),
-                _ => ProcessEnd(pattern, nums, cache),
-            };
-        }
-
-        private static long ProcessEnd(string _, ImmutableStack<int> nums, Cache __)
-        {
-            // the good case is when there are no numbers left at the end of the pattern
-            return nums.Any() ? 0 : 1;
-        }
-
-        private static long ProcessDot(string pattern, ImmutableStack<int> nums, Cache cache)
-        {
-            // consume one spring and recurse
-            return Compute(pattern[1..], nums, cache);
-        }
-
-        private static long ProcessQuestion(string pattern, ImmutableStack<int> nums, Cache cache)
-        {
-            // recurse both ways
-            return Compute("." + pattern[1..], nums, cache) + Compute("#" + pattern[1..], nums, cache);
-        }
-
-        private static long ProcessHash(string pattern, ImmutableStack<int> nums, Cache cache)
-        {
-            // take the first number and consume that many dead springs, recurse
-
-            if (!nums.Any())
-            {
-                return 0; // no more numbers left, this is no good
+             //   Log.WriteLine($"{In(indent)}{pattern}, {nums.ToReadable()}{CC.Frm} (cac) Got value: {value}{CC.Clr}");
+                return value;
             }
 
-            var n = nums.Peek();
-            nums = nums.Pop();
+            var c = pattern.FirstOrDefault();
+            switch (c)
+            {
+                case '.':
+                    // consume one spring and recurse
+                    Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('.') Consume one spring and recurse{CC.Clr}");
+                    cache[key] = Compute(pattern[1..], nums, cache, indent + 1);
+                 //   Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                    break;
+                case '?':
+                    // recurse both ways
+                    Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('?') Recurse both ways (for '.' and '#'){CC.Clr}");
+                    cache[key] = Compute("#" + pattern[1..], nums, cache, indent + 1) + Compute("." + pattern[1..], nums, cache, indent + 1);
+                 //   Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                    break;
+                case '#':
+                    if (!nums.Any())
+                    {
+                        Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('#') No more numbers left, this is no good{CC.Clr}");
+                        cache[key] = 0; // no more numbers left, this is no good
+                   //     Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                    }
+                    else
+                    {
+                        var n = nums.Peek();
+                        nums = nums.Pop();
 
-            var potentiallyDead = pattern.TakeWhile(s => s == '#' || s == '?').Count();
+                        var potentiallyDead = pattern.TakeWhile(s => s == '#' || s == '?').Count();
+                        if (potentiallyDead < n)
+                        {
+                            Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('#') Not enough dead springs {CC.Clr}");
+                            cache[key] = 0; // not enough dead springs 
+                    //        Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                        }
+                        else if (pattern.Length == n)
+                        {
+                            Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('#') pattern len == n, compute empty string {CC.Clr}");
+                            cache[key] = Compute("", nums, cache, indent + 1);
+                    //        Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                        }
+                        else if (pattern[n] == '#')
+                        {
+                            Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('#') dead spring follows the range -> not good {CC.Clr}");
+                            cache[key] = 0; // dead spring follows the range -> not good
+                    //        Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                        }
+                        else
+                        {
+                            Log.WriteLine($"{pattern.PadLeft(lineSize)}, {nums.ToReadable()}{CC.Frm} ('#') compute for (n + 1) {CC.Clr}");
+                            cache[key] = Compute(pattern[(n + 1)..], nums, cache, indent + 1);
+                         //   Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                        }
+                    }
+                    break;
+                default:
+                    // the good case is when there are no numbers left at the end of the pattern
+                    cache[key] = nums.Any() ? 0 : 1;
+                 //   Log.WriteLine($"{In(indent)}{key.pattern}, {key.nums.ToReadable()} => {cache[key]}");
+                    break;
+            }
 
-            if (potentiallyDead < n)
-            {
-                return 0; // not enough dead springs 
-            }
-            else if (pattern.Length == n)
-            {
-                return Compute("", nums, cache);
-            }
-            else if (pattern[n] == '#')
-            {
-                return 0; // dead spring follows the range -> not good
-            }
-            else
-            {
-                return Compute(pattern[(n + 1)..], nums, cache);
-            }
+            return cache[key];
         }
+
+
+
+        private static long Solve(string line, int repeat)
+        {
+            var parts = line.Split(" ");
+            var pattern = string.Join('?', Enumerable.Repeat(parts[0], repeat));
+            var nums = string.Join(',', Enumerable.Repeat(parts[1], repeat)).Split(',').Select(int.Parse);
+
+            var range = ImmutableStack.CreateRange(nums.Reverse());
+            var cache = new Cache();
+            lineSize = line.Length;
+            return Compute(pattern, range, cache);
+        }
+
+
+
+
+
+
 
         public static long Part1(string[] lines)
         {
-            return Solve(lines, 1);
-
+            Solve(lines[1], 1);
+            var sum = 0L;
+            foreach (var line in lines)
+            {
+            //    sum += Solve(line, 1);
+            }
+            return sum;
         }
         public static long Part2(string[] lines)
         {
-
-            return Solve(lines, 5);
+            var sum = 0L;
+            foreach (var line in lines)
+            {
+            //    sum += Solve(line, 5);
+            }
+            return sum;
         }
     }
 }
