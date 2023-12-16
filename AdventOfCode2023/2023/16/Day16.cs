9f6bf4f4c55021254aca3747334cc1d1fbbd6531
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2023-12-16 13:14:42 +0100

    object oriented, slow, cute map anim

--- AdventOfCode2023/2023/16/Day16.cs
+++ AdventOfCode2023/2023/16/Day16.cs
@@ -1,5 +1,6 @@
 ﻿#define DRAWMAPENABLED
 
+using Microsoft.VisualBasic.Logging;
 using System.Runtime.InteropServices;
 using System.Text;
 using StringSpan = System.ReadOnlySpan<char>;
@@ -7,7 +8,7 @@ using StringSpan = System.ReadOnlySpan<char>;
 namespace AdventOfCode2023
 {
     //[Force]                    // uncomment to force processing this type (regardless of which day it is according to DateTime)
-    //[AlwaysEnableLog]          // if uncommented, Log.Write() and Log.WriteLine() will still be honored in runs without a debugger (do not confuse with Debug/Release configuration)
+    [AlwaysEnableLog]          // if uncommented, Log.Write() and Log.WriteLine() will still be honored in runs without a debugger (do not confuse with Debug/Release configuration)
     //[DisableLogInDebug]        // if uncommented, Log will be disabled even when under debugger
     //[UseLiveDataInDeug]        // if uncommented and under a debug session, will use live data (problem data) instead of test data
     //[AlwaysUseTestData]        // if uncommented, will use test data in both debugging session and non-debugging session
@@ -15,6 +16,7 @@ namespace AdventOfCode2023
     [ExpectedTestAnswerPart2(51)] // if != 0, will report failure if expected answer != given answer
     class Day16
     {
+        private const int DrawMapDelay = 40;
         [Flags]
         enum BeamDirection : byte
         {
@@ -49,6 +51,109 @@ namespace AdventOfCode2023
                 }
             }
         }
+
+
+
+        class NewBeam
+        {
+            // current location, direction
+            public int X;
+            public int Y;
+            public BeamDirection Direction;
+            public void StepInDirection()
+            {
+                switch (Direction)
+                {
+                    case BeamDirection.Left: X--; break;
+                    case BeamDirection.Right: X++; break;
+                    case BeamDirection.Up: Y--; break;
+                    case BeamDirection.Down: Y++; break;
+                }
+            }
+            public void Update(StringSpan input, Span<byte> map, int width, int height, Queue<NewBeam> beamsToProcess)
+            {
+                var c = input.GetAt(X, Y, width, height, out var outOfBounds);
+                // beam has left the map
+                if (outOfBounds)
+                    return;
+                var m = map.GetAt(X, Y, width, height, out outOfBounds);
+
+                // if beam has already been here (with same direction)
+                // it means we have a loop. drop this beam
+                if ((m & (byte)Direction) == (byte)Direction)
+                    return;
+
+                // we 'energize' our spot
+                // and save our direction
+                map.SetAt((byte)(m | (byte)Direction), X, Y, width, height, out _);
+
+                // if our current place is empty space ('.'), continue in same direction
+                if (c == '.')
+                {
+                    StepInDirection();
+                    beamsToProcess.Enqueue(this);
+                    return;
+                }
+                if (c == '\\')
+                {
+                    Direction = Direction switch
+                    {
+                        BeamDirection.Right => BeamDirection.Down,
+                        BeamDirection.Left => BeamDirection.Up,
+                        BeamDirection.Up => BeamDirection.Left,
+                        BeamDirection.Down => BeamDirection.Right,
+                        _ => throw new NotImplementedException(),
+                    };
+                    StepInDirection();
+                    beamsToProcess.Enqueue(this);
+                    return;
+                }
+                if (c == '/')
+                {
+                    Direction = Direction switch
+                    {
+                        BeamDirection.Right => BeamDirection.Up,
+                        BeamDirection.Left => BeamDirection.Down,
+                        BeamDirection.Up => BeamDirection.Right,
+                        BeamDirection.Down => BeamDirection.Left,
+                        _ => throw new NotImplementedException(),
+                    };
+                    StepInDirection();
+                    beamsToProcess.Enqueue(this);
+                    return;
+                }
+
+                if (c == '-')
+                {
+                    if (Direction == BeamDirection.Up || Direction == BeamDirection.Down)
+                    {
+                        beamsToProcess.Enqueue(new NewBeam { X = X + 1, Y = Y, Direction = BeamDirection.Right });
+                        beamsToProcess.Enqueue(new NewBeam { X = X + - 1, Y = Y, Direction = BeamDirection.Left });
+                    }
+                    else
+                    {
+                        StepInDirection();
+                        beamsToProcess.Enqueue(this);
+                    }
+                    return;
+                }
+                if (c == '|')
+                {
+                    if (Direction == BeamDirection.Left || Direction == BeamDirection.Right)
+                    {
+                        beamsToProcess.Enqueue(new NewBeam { X = X, Y = Y + 1, Direction = BeamDirection.Down });
+                        beamsToProcess.Enqueue(new NewBeam { X = X, Y = Y - 1, Direction = BeamDirection.Up });
+                    }
+                    else
+                    {
+                        StepInDirection();
+                        beamsToProcess.Enqueue(this);
+                    }
+                    return;
+                }
+            }
+        }
+
 
 
         private static int dm_consoleTop;
@@ -88,9 +193,9 @@ namespace AdventOfCode2023
 
                     if (x == parameters.X && y == parameters.Y)
                         sb.Append($"{CC.HBg}");
-                    
+
                     sb.Append($"{colorFlag}{c}");
-                    
+
                     if (x == parameters.X && y == parameters.Y)
                         sb.Append($"{CC.Clr}");
                 }
@@ -105,6 +210,48 @@ namespace AdventOfCode2023
             Thread.Sleep(sleep);
 #endif
         }
+
+        private static void DrawMap(StringSpan input, Span<byte> map, int width, int heigth, List<(int, int)> currentPositions, int sleep = 0)
+        {
+#if DRAWMAPENABLED
+            if (Log.Enabled == false) return;
+            var sb = new StringBuilder();
+
+            for (int y = 0; y < heigth; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var c = input.GetAt(x, y, width, heigth, out _);
+                    var m = map.GetAt(x, y, width, heigth, out _);
+
+                    // replace dot because our terminal (and Cascadia Nerd Cove font)
+                    // shows 3 dots (...) as it's own glyph, which makes it a bit harder to read
+                    if (c == '.') c = CC.DotReplacement;
+
+                    var colorFlag = CC.Frm;
+                    if (m != 0)
+                        colorFlag = CC.Sys;
+
+                    if(currentPositions.Contains((x, y)))
+                        sb.Append($"{CC.HBg}");
+
+                    sb.Append($"{colorFlag}{c}");
+
+                    if (currentPositions.Contains((x, y)))
+                        sb.Append($"{CC.Clr}");
+                }
+
+                sb.AppendLine($"{CC.Clr}");
+            }
+
+            Console.CursorTop = dm_consoleTop;
+            Console.CursorLeft = 0;
+            Log.WriteLine(sb.ToString());
+
+            Thread.Sleep(sleep);
+#endif
+        }
+
 
         private static void ProcessBeam(ProcessParameters parameters)
         {
@@ -135,7 +282,7 @@ namespace AdventOfCode2023
                 if (c == '.')
                 {
                     parameters.StepInDirection();
-                    DrawMap(parameters, 1000);
+                    DrawMap(parameters, DrawMapDelay);
                     continue;
                 }
 
@@ -155,7 +302,7 @@ namespace AdventOfCode2023
                         _ => throw new NotImplementedException(),
                     };
                     parameters.StepInDirection();
-                    DrawMap(parameters, 1000);
+                    DrawMap(parameters, DrawMapDelay);
 
                     continue;
                 }
@@ -174,7 +321,7 @@ namespace AdventOfCode2023
                         _ => throw new NotImplementedException(),
                     };
                     parameters.StepInDirection();
-                    DrawMap(parameters, 1000);
+                    DrawMap(parameters, DrawMapDelay);
 
                     continue;
                 }
@@ -197,7 +344,7 @@ namespace AdventOfCode2023
                             Input = parameters.Input,
                             Map = parameters.Map
                         };
-                        DrawMap(param1, 1000);
+                        DrawMap(param1, DrawMapDelay);
                         ProcessBeam(param1);
 
                         var param2 = new ProcessParameters
@@ -210,14 +357,14 @@ namespace AdventOfCode2023
                             Input = parameters.Input,
                             Map = parameters.Map
                         };
-                        DrawMap(param2, 1000);
+                        DrawMap(param2, DrawMapDelay);
                         ProcessBeam(param2);
 
                         return;
                     }
                     // else just continue
                     parameters.StepInDirection();
-                    DrawMap(parameters, 1000);
+                    DrawMap(parameters, DrawMapDelay);
                     continue;
                 }
                 if (c == '|')
@@ -250,18 +397,18 @@ namespace AdventOfCode2023
                     }
                     // else just continue
                     parameters.StepInDirection();
-                    DrawMap(parameters, 1000);
+                    DrawMap(parameters, DrawMapDelay);
                     continue;
                 }
             }
         }
 
         private static byte[]? _mapMemory = null;
-        private static unsafe long EnergizeMapWithBeam(StringSpan lines, int width, int heigth, int beamStartX, int beamStartY, BeamDirection initialDirection)
+        private static unsafe long EnergizeMapWithBeam(StringSpan lines, int width, int height, int beamStartX, int beamStartY, BeamDirection initialDirection)
         {
             // need a field map
-            if (_mapMemory == null || _mapMemory.Length != (width * heigth))
-                _mapMemory = new byte[width * heigth];
+            if (_mapMemory == null || _mapMemory.Length != (width * height))
+                _mapMemory = new byte[width * height];
 
             // we could also use stackalloc like this:
             // var map2 = stackalloc byte[width * height];
@@ -274,18 +421,40 @@ namespace AdventOfCode2023
             map.Clear(); // clear map after last use
 
             InitDrawMap();
-            // process with beam from starting point (0, 0), to the right
-            ProcessBeam(new ProcessParameters
+
+
+            // just for fun, we'll do a object way, with Update() method instead of
+            // recursion
+
+            NewBeam initial = new() { X = beamStartX, Y = beamStartY, Direction = initialDirection };
+            Queue<NewBeam> beams = new();
+            beams.Enqueue(initial);
+            Queue<NewBeam> beams2 = new();
+            while (beams.Count > 0)
             {
-                Direction = initialDirection,
-                X = (short)beamStartX,
-                Y = (short)beamStartY,
-                Height = (short)heigth,
-                Width = (short)width,
-                Input = lines,
-                Map = map
-            });
+                while (beams.TryDequeue(out var beam))
+                    beam.Update(lines, map, width, height, beams2);
+
+                var positions = beams2.Select(b => (b.X, b.Y));
+                DrawMap(lines, map, width, height, positions.ToList(), sleep: DrawMapDelay);
+                (beams, beams2) = (beams2, beams);
+            }
+
             CloseDrawMap();
+
+
+            //// process with beam from starting point (0, 0), to the right
+            //ProcessBeam(new ProcessParameters
+            //{
+            //    Direction = initialDirection,
+            //    X = (short)beamStartX,
+            //    Y = (short)beamStartY,
+            //    Height = (short)height,
+            //    Width = (short)width,
+            //    Input = lines,
+            //    Map = map
+            //});
+            //CloseDrawMap();
 
             // now, simply count marked positions
             var sum = 0L;
@@ -307,6 +476,8 @@ namespace AdventOfCode2023
         // change to string or string[] to get other types of input
         public static long Part2(StringSpan input, int lineWidth, int count)
         {
+            Log.Enabled = false;
+
             // our method is fast enough to just search for each possibility independently
             // we could try to keep the same map from steps before, and skip 
             // beams that were already traversed
