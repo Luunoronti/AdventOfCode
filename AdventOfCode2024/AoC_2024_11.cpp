5b5affa17891b459f6080051e060ab7aacc79d6f
Luunoronti <wiktoryn@hotmail.com>
2024-12-11 18:18:57 +0100

    1

--- AdventOfCode2024/AoC_2024_11.cpp
+++ AdventOfCode2024/AoC_2024_11.cpp
@@ -6,9 +6,13 @@ using namespace aoc;
 
 std::unordered_map<int64_t, std::pair<int64_t, int64_t>> doubleDigitCache;
 
+int64_t totalLogs = 0;
+int64_t totalPows = 0;
+int64_t maxLogsPerRun = 0;
 int64_t AoC_2024_11::AdvanceOneStep(Map* map, Map* target)
 {
     int64_t sum = 0;
+    int64_t logsThisRun = 0;
     for(const auto& pair : *map)
     {
         const auto& number = pair.first;
@@ -29,22 +33,29 @@ int64_t AoC_2024_11::AdvanceOneStep(Map* map, Map* target)
             continue;
         }
 
+        // add this number to an array for avx2 parallel processing
+        // we will have as many vecotrs as ve have cores
+        // and each vector will be processed 
+        totalLogs++;
+        logsThisRun++;
         int64_t numDigits = static_cast<int64_t>(log10(number)) + 1;
         if(!(numDigits % 2))
         {
+            totalPows++;
             int64_t divisor = static_cast<int64_t>(std::pow(10, numDigits / 2));
             const auto p = std::pair<int64_t, int64_t>(number / divisor, number % divisor);
             (*target)[p.first] += count;
             (*target)[p.second] += count;
             doubleDigitCache[number] = p;
             sum += count * 2;
+            
+            continue;
         }
-        else
-        {
-            (*target)[number * 2024] += count;
-            sum += count;
-        }
+
+        (*target)[number * 2024] += count;
+        sum += count;
     }
+    maxLogsPerRun = max(maxLogsPerRun, logsThisRun);
     return sum;
 }
 void AoC_2024_11::CountAll(const vector<int64_t>& list, int steps, int stepsForPart1, Map& map, Map& map2)
@@ -77,6 +88,7 @@ const int64_t AoC_2024_11::Step1()
     Map map, map2;
     CountAll(list, 75, 25, map, map2);
 
+    cout << "Total logs: " << totalLogs << ", total pows: " << totalPows << ", max logs per run: " << maxLogsPerRun << endl;
     return IsTest() ? firstPart_Test : firstPart_Live;
 };
 const int64_t AoC_2024_11::Step2()
