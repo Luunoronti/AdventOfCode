a584199e19ea52eef3e4faf70e48a8305213c6a3
Luunoronti <wiktoryn@hotmail.com>
2025-10-23 19:20:08 +0200

    .

---
+++ AoC.Visualizers/Grid/GridVisualizer.cs
@@ -0,0 +1,43 @@
+﻿//using System.Windows.Forms;
+//using Microsoft.VisualStudio.DebuggerVisualizers;
+//using AoC.Core;
+//using System.Diagnostics;
+//using System.Windows.Controls;
+
+//[assembly: DebuggerVisualizer(
+//    typeof(AoC.Visualizers.GridVisualizer),
+//    typeof(VisualizerObjectSource),
+//    Target = typeof(Grid<int>),
+//    Description = "Grid Visualizer")]
+
+//namespace AoC.Visualizers;
+
+//public class GridVisualizer : DialogDebuggerVisualizer
+//{
+//    protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+//    {
+//        if (windowService == null || objectProvider == null)
+//            return;
+
+//        var grid = (Grid<int>)objectProvider.GetObject();
+
+//        // Simple WinForms UI
+//        var form = new Form { Text = "Grid Visualizer", Width = 400, Height = 400 };
+//        var textBox = new TextBox
+//        {
+//            Multiline = true,
+//            Dock = DockStyle.Fill,
+//            Font = new System.Drawing.Font("Consolas", 12)
+//        };
+
+//        for (int y = 0; y < grid.Height; y++)
+//        {
+//            for (int x = 0; x < grid.Width; x++)
+//                textBox.AppendText($"{grid[x, y],3}");
+//            textBox.AppendText(Environment.NewLine);
+//        }
+
+//        form.Controls.Add(textBox);
+//        windowService.ShowDialog(form);
+//    }
+//}
---
+++ AoC.Visualizers/Grid/GridVisualizerUserControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.UI;
+using Newtonsoft.Json.Linq;
+
+namespace AoC.Visualizers;
+
+
+
+/// <summary>
+/// Remote user control to visualize the <see cref="Match"/> value.
+/// </summary>
+internal class GridVisualizerUserControl : RemoteUserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GridVisualizerUserControl"/> class.
+    /// </summary>
+    /// <param name="dataContext">Data context of the remote control.</param>
+    public GridVisualizerUserControl(RegexMatch dataContext)
+        : base(dataContext)
+    {
+    }
+}
+
+
+
+
+
+[DataContract]
+internal class RegexMatch : RegexGroup
+{
+    [DataMember]
+    public RegexGroup[]? Groups { get; set; }
+}
+
+[DataContract]
+internal class RegexGroup : RegexCapture
+{
+    [DataMember]
+    public bool Success { get; set; }
+
+    [DataMember]
+    public RegexCapture[]? Captures { get; set; }
+}
+
+[DataContract]
+internal class RegexCapture
+{
+    [DataMember]
+    public int Index { get; set; }
+
+    [DataMember]
+    public int Length { get; set; }
+
+    [DataMember]
+    public string? Value { get; set; }
+
+    [DataMember]
+    public string? Name { get; set; }
+
+#if VISUALIZER
+    [DataMember]
+    public string Range => $"{this.Index}-{this.Index + this.Length}";
+#endif
+}
