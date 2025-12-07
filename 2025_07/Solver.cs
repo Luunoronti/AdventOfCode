9310b223b2a6602455d0e7e57179bf0b46c2efa8
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2025-12-07 20:23:22 +0100

    .

--- 2025_07/Solver.cs
+++ 2025_07/Solver.cs
@@ -7,40 +7,53 @@ public static class Solver
     [ExpectedResult("live", 1703)]
     public static long SolvePart1(string[] Lines)
     {
+        // dimensions
         var height = Lines.Length;
         var width = Lines[0].Length;
 
+        // look for start position
         var startColumn = 0;
-
         var line = Lines[0].AsSpan();
-
         while (startColumn < height && line[startColumn++] != 'S') ;
         startColumn--;
 
+        // state
         Span<byte> active = stackalloc byte[width];
         Span<byte> next = stackalloc byte[width];
         var splits = 0L;
 
+        // initialize beam
         active.Clear();
         active[startColumn] = 1;
 
+        // cascade the beam down
         for (var row = 1; row < height; row++)
         {
             next.Clear();
             line = Lines[row].AsSpan();
+            
+            // look for splitters
             for (var col = 0; col < width; col++)
             {
+                // there is no beam in this cell, so we have nothing to do
                 if (active[col] == 0) continue;
+
+                // no splitter here, so we just let the beam travel
                 if (line[col] != '^')
                 {
                     next[col] = 1;
                     continue;
                 }
+
+                // there is a split. record it
                 splits++;
 
+                // and spawn two new beams, to the left and right
                 if (col > 0) next[col - 1] = 1;
                 if (col + 1 < width) next[col + 1] = 1;
             }
+
+            // swap active row and next row
             var temp = active;
             active = next;
             next = temp;
