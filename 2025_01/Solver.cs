04a0f0388e94b95e8916fd3c417cc80f1b819ae6
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2025-12-07 16:01:18 +0100

    .

--- 2025_01/Solver.cs
+++ 2025_01/Solver.cs
@@ -102,3 +102,10 @@ public static class Solver
     [ExpectedResult("live", 6498)]
     public static long SolvePart2(string[] Lines) => Solution(Lines, true);
 }
+
+/*
+| Method | Mean     | Mean     | Error     | StdDev    | Median   | Allocated |
+|------- |---------:|---------:|----------:|----------:|---------:|----------:|
+| Part1  | 0.004 ms | 3.664 us | 0.0091 us | 0.0085 us | 3.662 us |         - |
+| Part2  | 0.005 ms | 5.161 us | 0.0193 us | 0.0181 us | 5.160 us |         - |
+*/
